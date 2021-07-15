2360753a7edf0af4b0a7c8c578694725a26d1599
Rchardon <33094562+Rchardon@users.noreply.github.com>
2021-07-15 23:58:43 +0200

    refacto openantibirthdoor

--- src/features/optional/quality/openAntibirthDoor.ts
+++ src/features/optional/quality/openAntibirthDoor.ts
@@ -1,14 +1,14 @@
 // Automatically open Antibirth doors in races to Mother
 
 import g from "../../../globals";
+import { getAllDoors } from "../../../misc";
 
 export function main(): void {
   const player = Isaac.GetPlayer();
   if (g.r.GetType() === RoomType.ROOM_BOSS && g.r.IsClear()) {
-    for (let i = 0; i < 8; i++) {
-      const Door = g.r.GetDoor(i);
-      if (Door !== null && Door.IsLocked()) {
-        Door.TryUnlock(player, true);
+    for (const door of getAllDoors()) {
+      if (door !== null && door.IsLocked()) {
+        door.TryUnlock(player, true);
       }
     }
   }
