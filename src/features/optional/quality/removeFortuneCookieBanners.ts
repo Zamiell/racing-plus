5e03e06b6d423e5390a903a98f695d655b686b11
James <5511220+Zamiell@users.noreply.github.com>
2021-07-24 19:28:03 -0400

    Update removeFortuneCookieBanners.ts

--- src/features/optional/quality/removeFortuneCookieBanners.ts
+++ src/features/optional/quality/removeFortuneCookieBanners.ts
@@ -1,8 +1,15 @@
 export function useItem(): void {
-  const hud = g.g.GetHUD();
+  const HUD = g.g.GetHUD();
 
-  // This overrides the fortune cookie banner
-  if (!VanillaStreakText) {
-    hud.ShowItemText("");
+  // The Fortune Cookie text is annoying and blocks gameplay,
+  // so we want to remove it entirely
+  // Racing+ removes the files that show the vanilla streak text
+  // Thus, we can override the Fortune Cookie text by immediately showing normal streak text after a
+  // Fortune Cookie use
+  // However, this will only work if they do not have the "VanillaStreakText" mod enabled
+  if (VanillaStreakText) {
+    return
   }
+
+  HUD.ShowItemText("");
 }
