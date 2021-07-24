3c508de8d3d8724a08adfaa3e12c61c01452897d
Gamonymous <33094562+Rchardon@users.noreply.github.com>
2021-07-24 22:28:57 +0200

    Open MS door on MS goal (#12)
    * Open MS door on MS goal
    
    * Update openMegaSatanDoor.ts
    
    * Update openMegaSatanDoor.ts
    
    * Move logic to the feature file
    
    * Move feature from optional to race
    
    * flip the logic
    
    * add stage 11 early check
    
    * Add feature in callbacks
    
    * fix check
    
    * Update postNewLevel.ts
    
    Co-authored-by: James <5511220+Zamiell@users.noreply.github.com>

---
+++ src/features/race/megaSatan.ts
@@ -0,0 +1,22 @@
+// Automatically open the Mega Satan door on races with a Mega Satan goal
+
+import g from "../../globals";
+
+export function postNewLevel(): void {
+  const stage = g.l.GetStage();
+  const player = Isaac.GetPlayer();
+
+  if (
+    g.race.status !== "in progress" ||
+    g.race.myStatus !== "racing" ||
+    g.race.goal !== "Mega Satan" ||
+    stage !== 11
+  ) {
+    return;
+  }
+
+  const topDoor = g.r.GetDoor(1);
+  if (topDoor !== null) {
+    topDoor.TryUnlock(player, true);
+  }
+}
