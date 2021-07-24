de84433df90c0bf28655c3a9f6b3d7fca0b9f489
Rchardon <33094562+Rchardon@users.noreply.github.com>
2021-07-24 21:50:18 +0200

    update imports

--- src/features/race/openAntibirthDoor.ts
+++ src/features/race/openAntibirthDoor.ts
@@ -1,7 +1,7 @@
 // Automatically open Antibirth doors in races to Mother
 
-import g from "../../../globals";
-import { getAllDoors } from "../../../misc";
+import g from "../../globals";
+import { getAllDoors } from "../../misc";
 
 export function main(): void {
   if (
