e0560c3966fcb77d5f2fa6c2cbc2b7245d8effeb
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-28 14:26:23 -0400

    fix for non functioning code

--- src/features/race/removeStrangeDoor.ts
+++ src/features/race/removeStrangeDoor.ts
@@ -1,13 +1,13 @@
 import { MAX_NUM_DOORS } from "../../constants";
 import g from "../../globals";
 import log from "../../log";
-import { isAntibirthStage } from "../../misc";
+import { getRoomIndex, isAntibirthStage } from "../../misc";
 
 export function postNewRoom(): void {
   const stage = g.l.GetStage();
   const antibirthStage = isAntibirthStage();
-  const strangeDoor = GridRooms.ROOM_SECRET_EXIT_IDX;
   const startingRoomIndex = g.l.GetStartingRoomIndex();
+  const roomIndex = getRoomIndex();
 
   if (
     g.race.status !== "in progress" ||
@@ -15,14 +15,14 @@ export function postNewRoom(): void {
     g.race.goal !== "The Beast" ||
     antibirthStage ||
     stage !== 6 ||
-    strangeDoor !== startingRoomIndex
+    roomIndex !== startingRoomIndex
   ) {
     return;
   }
 
   for (let i = 0; i < MAX_NUM_DOORS; i++) {
     const door = g.r.GetDoor(i);
-    if (door !== null && door.TargetRoomIndex === strangeDoor) {
+    if (door !== null && door.TargetRoomIndex === GridRooms.ROOM_SECRET_EXIT_IDX) {
       g.r.RemoveDoor(i);
       log("Manually removed the strange door (for races going to The Beast).");
     }
