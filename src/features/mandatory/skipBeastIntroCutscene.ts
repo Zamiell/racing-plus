64111b1744f549e429a298c85935028c40aa548f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-28 16:03:01 -0400

    updating deps + fixing enum

--- src/features/mandatory/skipBeastIntroCutscene.ts
+++ src/features/mandatory/skipBeastIntroCutscene.ts
@@ -3,7 +3,7 @@ import { consoleCommand } from "../../misc";
 export function postEntityKillDogma(entity: Entity): void {
   // As soon as the player kills the second phase of Dogma,
   // warp them immediately to The Beast fight without playing the cutscene
-  if (entity.Variant === DogmaVariant.ANGEL) {
+  if (entity.Variant === DogmaVariant.ANGEL_PHASE_2) {
     consoleCommand("goto x.itemdungeon.666");
   }
 }
