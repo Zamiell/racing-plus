b37beee13548d042eef34bed20de44977d0bc4ad
Rchardon <33094562+Rchardon@users.noreply.github.com>
2021-07-29 19:35:53 +0200

    Update removeAntibirthDoor.ts

--- src/features/race/removeAntibirthDoor.ts
+++ src/features/race/removeAntibirthDoor.ts
@@ -9,28 +9,7 @@ export function postRoomClear(): void {
 }
 
 export function postNewRoom(): void {
-  const stage = g.l.GetStage();
-  const antibirthStage = isAntibirthStage();
-  const roomType = g.r.GetType();
-
-  if (
-    g.race.status !== "in progress" ||
-    g.race.myStatus !== "racing" ||
-    g.race.goal !== "The Beast" ||
-    roomType !== RoomType.ROOM_BOSS ||
-    ((!antibirthStage || stage !== 4) && (antibirthStage || stage !== 5))
-  ) {
-    return;
-  }
-
-  for (const door of getAllDoors()) {
-    if (door.TargetRoomIndex === GridRooms.ROOM_SECRET_EXIT_IDX) {
-      g.r.RemoveDoor(door.Slot);
-      log(
-        "Manually removed the Mausoleum door (for races going to The Beast).",
-      );
-    }
-  }
+  removeAntibirthDoor();
 }
 
 function removeAntibirthDoor() {
