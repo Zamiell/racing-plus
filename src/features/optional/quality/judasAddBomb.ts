099e029758655c60ae3afd5806fd0ff610763306
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-10 17:51:02 -0400

    bad comment

--- src/features/optional/quality/judasAddBomb.ts
+++ src/features/optional/quality/judasAddBomb.ts
@@ -1,5 +1,4 @@
 // We grant Judas an extra bomb so that the character is slightly more skill based
-// The same mechanic also applies to Dark Judas
 
 import { getPlayers } from "isaacscript-common";
 import { config } from "../../../modConfigMenu";
