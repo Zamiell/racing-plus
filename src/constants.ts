f8f90b4b9c88c55dfff1fe72b8ebeba5a5ab93f0
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-17 23:51:35 -0400

    v0.59.3

--- src/constants.ts
+++ src/constants.ts
@@ -14,4 +14,4 @@ export const SPRITE_TAINTED_BETHANY_OFFSET = Vector(0, 6);
 export const TAINTED_KEEPER_ITEM_PRICE = 15;
 
 /** The version is updated automatically by the IsaacScript publisher. */
-export const VERSION = "0.59.2";
+export const VERSION = "0.59.3";
