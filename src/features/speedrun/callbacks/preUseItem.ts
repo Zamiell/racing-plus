db6113899c9aee6251c9bb8c68526c4b386d07fe
Rchardon <33094562+Rchardon@users.noreply.github.com>
2021-08-19 00:13:41 +0200

    Update preUseItem.ts

--- src/features/speedrun/callbacks/preUseItem.ts
+++ src/features/speedrun/callbacks/preUseItem.ts
@@ -1,5 +1,5 @@
 import * as season1 from "../season1";
 
-export function preUseWeNeedToGoDeeper(rng: RNG): boolean | void {
-  return season1.preUseWeNeedToGoDeeper(rng);
+export function preUseItemWeNeedToGoDeeper(rng: RNG): boolean | void {
+  return season1.preUseItemWeNeedToGoDeeper(rng);
 }
