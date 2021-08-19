f373add0abe666b08ba3e20e318d76db7b973729
James <5511220+Zamiell@users.noreply.github.com>
2021-08-19 12:37:16 -0400

    Update reimplementShovel.ts

--- src/features/speedrun/reimplementShovel.ts
+++ src/features/speedrun/reimplementShovel.ts
@@ -1,16 +1,14 @@
 import g from "../../globals";
 
 export function spawnTrapdoor(rng: RNG): void {
-  // Get a random value between 0 and 1 that will determine what kind of trapdoor we'll get
-  const trapdoorPercent = rng.RandomFloat();
-  const player = Isaac.GetPlayer();
   const stage = g.l.GetStage();
-  const playerPosition = player.Position;
-  const trapDoorType =
+
+  const trapdoorChance = rng.RandomFloat();
+  const gridEntityType =
     trapdoorPercent <= 0.1
       ? GridEntityType.GRID_STAIRS
       : GridEntityType.GRID_TRAPDOOR;
-
+  
   player.AnimateCollectible(
     CollectibleType.COLLECTIBLE_WE_NEED_TO_GO_DEEPER,
     "UseItem",
@@ -21,5 +19,5 @@ export function spawnTrapdoor(rng: RNG): void {
     return;
   }
 
-  Isaac.GridSpawn(trapDoorType, 0, playerPosition, true);
+  Isaac.GridSpawn(gridEntityType, 0, player.Position, true);
 }
