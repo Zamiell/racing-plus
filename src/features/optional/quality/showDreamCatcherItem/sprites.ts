09d4a64e825a32efea988ce9078c1458e32fe908
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-20 18:03:12 -0400

    bugfix for dream catcher sprites in save data manager

--- src/features/optional/quality/showDreamCatcherItem/sprites.ts
+++ src/features/optional/quality/showDreamCatcherItem/sprites.ts
@@ -12,27 +12,33 @@ import v from "./v";
 // Near the top-left of the room
 const SPRITE_SPACING = 30;
 
+let dreamCatcherSprite = Sprite();
+let itemSprites: Sprite[] = [];
+let bossSprites: Sprite[] = [];
+
+export function init(): void {
+  dreamCatcherSprite = initGlowingItemSprite(
+    CollectibleType.COLLECTIBLE_DREAM_CATCHER,
+  );
+}
+
 export function set(): void {
   if (!shouldShowSprites()) {
     reset();
     return;
   }
 
-  v.level.dreamCatcherSprite = initGlowingItemSprite(
-    CollectibleType.COLLECTIBLE_DREAM_CATCHER,
-  );
-
   for (let i = 0; i < v.level.items.length; i++) {
-    if (v.level.itemSprites[i] === null) {
+    if (itemSprites[i] === null) {
       const collectibleType = v.level.items[i];
-      v.level.itemSprites[i] = initGlowingItemSprite(collectibleType);
+      itemSprites[i] = initGlowingItemSprite(collectibleType);
     }
   }
 
   for (let i = 0; i < v.level.bosses.length; i++) {
-    if (v.level.bossSprites[i] === null) {
+    if (bossSprites[i] === null) {
       const [entityType, variant] = v.level.bosses[i];
-      v.level.bossSprites[i] = initBossSprite(entityType, variant);
+      bossSprites[i] = initBossSprite(entityType, variant);
     }
   }
 }
@@ -55,9 +61,8 @@ function initBossSprite(entityType: EntityType, variant: int) {
 }
 
 export function reset(): void {
-  v.level.dreamCatcherSprite = null;
-  v.level.itemSprites = [];
-  v.level.bossSprites = [];
+  itemSprites = [];
+  bossSprites = [];
 }
 
 function shouldShowSprites() {
@@ -86,14 +91,13 @@ export function draw(): void {
   const topLeftRoomPosition = gridToPos(1, 1);
   const nextToDreamCatcherPosition = gridToPos(2, 1);
 
-  if (v.level.dreamCatcherSprite !== null) {
-    const sprite = v.level.dreamCatcherSprite;
+  {
     const renderPosition = Isaac.WorldToRenderPosition(topLeftRoomPosition);
-    sprite.RenderLayer(0, renderPosition);
+    dreamCatcherSprite.RenderLayer(0, renderPosition);
   }
 
-  for (let i = 0; i < v.level.itemSprites.length; i++) {
-    const sprite = v.level.itemSprites[i];
+  for (let i = 0; i < itemSprites.length; i++) {
+    const sprite = itemSprites[i];
     const renderPosition = Isaac.WorldToRenderPosition(
       nextToDreamCatcherPosition,
     );
@@ -103,12 +107,12 @@ export function draw(): void {
     sprite.RenderLayer(0, position);
   }
 
-  for (let i = 0; i < v.level.bossSprites.length; i++) {
-    const sprite = v.level.bossSprites[i];
+  for (let i = 0; i < bossSprites.length; i++) {
+    const sprite = bossSprites[i];
     const renderPosition = Isaac.WorldToRenderPosition(
       nextToDreamCatcherPosition,
     );
-    const numRightShifts = i + v.level.itemSprites.length;
+    const numRightShifts = i + itemSprites.length;
     const positionAdjustment = Vector(SPRITE_SPACING * numRightShifts, 0);
     const position = renderPosition.add(positionAdjustment);
     sprite.RenderLayer(0, position);
