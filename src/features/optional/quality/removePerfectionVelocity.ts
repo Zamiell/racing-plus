c2d037c1b2c93f1ed612b2ebcf5d6f44e7beea43
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-21 11:53:12 -0400

    bugfix for perfection

--- src/features/optional/quality/removePerfectionVelocity.ts
+++ src/features/optional/quality/removePerfectionVelocity.ts
@@ -1,9 +1,24 @@
 // Since the Perfection trinket spawns with a velocity,
 // it can sometimes go over pits and become inaccessible
 
+import { saveDataManager } from "isaacscript-common";
 import g from "../../../globals";
 import { config } from "../../../modConfigMenu";
 import { findFreePosition } from "../../../utilGlobals";
+
+const v = {
+  run: {
+    spawnedPerfection: false,
+  },
+};
+
+export function init(): void {
+  saveDataManager("removePerfectionVelocity", v, featureEnabled);
+}
+
+function featureEnabled() {
+  return config.removePerfectionVelocity;
+}
 
 // ModCallbacks.MC_POST_PICKUP_INIT (34)
 // PickupVariant.PICKUP_TRINKET (350)
@@ -16,10 +31,16 @@ export function postPickupInitTrinket(pickup: EntityPickup): void {
     return;
   }
 
+  if (v.run.spawnedPerfection) {
+    return;
+  }
+
   // Normally, the Perfection trinket will be flung outward from the location of the boss
   // Instead, set it to be a free tile near the center of the room
   // This is guaranteed to be accessible because there are no boss rooms that have divided islands
   const centerPos = g.r.GetCenterPos();
   pickup.Position = findFreePosition(centerPos);
   pickup.Velocity = Vector.Zero;
+
+  v.run.spawnedPerfection = true;
 }
