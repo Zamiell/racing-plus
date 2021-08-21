5a1c353a338a36adc3a92efc8a3731bddc01b833
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-21 11:55:41 -0400

    bugfix for not initializing feature

--- src/initFeatureVariables.ts
+++ src/initFeatureVariables.ts
@@ -24,6 +24,7 @@ import * as fastTravelVars from "./features/optional/major/fastTravel/v";
 import * as freeDevilItem from "./features/optional/major/freeDevilItem";
 import * as startWithD6 from "./features/optional/major/startWithD6";
 import * as automaticItemInsertion from "./features/optional/quality/automaticItemInsertion/automaticItemInsertion";
+import * as removePerfectionVelocity from "./features/optional/quality/removePerfectionVelocity";
 import * as showDreamCatcherItemVars from "./features/optional/quality/showDreamCatcherItem/v";
 import * as showEdenStartingItems from "./features/optional/quality/showEdenStartingItems";
 import * as showMaxFamiliars from "./features/optional/quality/showMaxFamiliars";
@@ -84,6 +85,7 @@ export default function initFeatureVariables(): void {
   showMaxFamiliars.init();
   speedUpFadeIn.init();
   automaticItemInsertion.init();
+  removePerfectionVelocity.init();
 
   // Gameplay changes
   combinedDualityDoors.init();
