498ab862ae82a0bb6a0f2b70757297e9b08129e1
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-29 10:57:24 -0400

    removing livesplit globals

--- src/features/speedrun/speedrunTimer.ts
+++ src/features/speedrun/speedrunTimer.ts
@@ -3,19 +3,6 @@ import * as timer from "../../timer";
 import TimerType from "../../types/TimerType";
 import v from "./v";
 
-const MAX_ELAPSED_FRAME_DIGITS = 6;
-const LIVESPLIT_VARIABLE_PREFIX = "Krakenos";
-const LIVESPLIT_VARIABLE_SUFFIX = "Polish";
-
-// We write the elapsed frames to a global variable so that LiveSplit can reach into the game's
-// memory and find out what it is
-declare let SpeedrunTimerString: string;
-const zeros = "".padStart(MAX_ELAPSED_FRAME_DIGITS, "0");
-SpeedrunTimerString =
-  LIVESPLIT_VARIABLE_PREFIX + zeros + LIVESPLIT_VARIABLE_SUFFIX;
-declare let SpeedrunTimerNumber: int;
-SpeedrunTimerNumber = 0;
-
 export function postRender(): void {
   checkDisplay();
 }
@@ -35,14 +22,4 @@ function checkDisplay() {
   const seconds = elapsedFrames / ISAAC_FRAMES_PER_SECOND;
 
   timer.display(TimerType.RACE_OR_SPEEDRUN, seconds);
-  notifyLiveSplit(elapsedFrames);
-}
-
-function notifyLiveSplit(elapsedFrames: int) {
-  const paddedFrames = elapsedFrames.toString().padStart(6, "0");
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  SpeedrunTimerString =
-    LIVESPLIT_VARIABLE_PREFIX + paddedFrames + LIVESPLIT_VARIABLE_SUFFIX;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  SpeedrunTimerNumber = elapsedFrames;
 }
