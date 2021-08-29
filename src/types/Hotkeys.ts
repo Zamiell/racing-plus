49a150a9db2cada5ea6ad9ff6b1bc642f445c34c
Gamonymous <33094562+Rchardon@users.noreply.github.com>
2021-08-29 18:01:26 +0200

    remove fast drop controller hotkeys (#45)

--- src/types/Hotkeys.ts
+++ src/types/Hotkeys.ts
@@ -1,8 +1,5 @@
 export default class Hotkeys {
   fastDropAllKeyboard = -1;
-  fastDropAllController = -1;
   fastDropTrinketsKeyboard = -1;
-  fastDropTrinketsController = -1;
   fastDropPocketKeyboard = -1;
-  fastDropPocketController = -1;
 }
