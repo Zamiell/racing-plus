4a459aa2eab398ab48874d9ab1beda08e6e9e284
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-09-05 13:49:22 -0400

    bugfix for scared heart

--- src/racing-plus/features/optional/graphics/scaredHeart.ts
+++ src/racing-plus/features/optional/graphics/scaredHeart.ts
@@ -5,6 +5,10 @@ export function postPickupInitHeart(pickup: EntityPickup): void {
     return;
   }
 
+  if (pickup.SubType !== HeartSubType.HEART_SCARED) {
+    return;
+  }
+
   const sprite = pickup.GetSprite();
   const animation = sprite.GetAnimation();
   sprite.Load("gfx/005.020_scared heart custom.anm2", true);
