e903775e20482aeac5be121039029bdca7971e71
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-09-10 20:30:35 -0400

    s1 not beta

--- src/racing-plus/features/speedrun/enums.ts
+++ src/racing-plus/features/speedrun/enums.ts
@@ -1,7 +1,4 @@
-const SEASON_1_NAME = "R+7 Season 1 Beta";
-const CHANGE_CHAR_ORDER_NAME = "Change Char Order";
-
 export enum ChallengeCustom {
-  SEASON_1 = Isaac.GetChallengeIdByName(SEASON_1_NAME),
-  CHANGE_CHAR_ORDER = Isaac.GetChallengeIdByName(CHANGE_CHAR_ORDER_NAME),
+  SEASON_1 = Isaac.GetChallengeIdByName("R+7 Season 1"),
+  CHANGE_CHAR_ORDER = Isaac.GetChallengeIdByName("Change Char Order"),
 }
