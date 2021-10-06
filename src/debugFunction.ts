a472b5b0d64552a0d71a5291e875ab6ccfc8beea
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-10-05 20:43:23 -0400

    bugfix for angel room #14

--- src/debugFunction.ts
+++ src/debugFunction.ts
@@ -1,4 +1,4 @@
-import { isKeyboardPressed, round } from "isaacscript-common";
+import { isKeyboardPressed } from "isaacscript-common";
 import g from "./globals";
 import { hotkeys } from "./modConfigMenu";
 
@@ -48,12 +48,14 @@ function hotkey2Function() {
 
 // ModCallbacks.MC_POST_FAMILIAR_RENDER (25)
 export function postFamiliarRender(_familiar: EntityFamiliar): void {
-  // const text = `State: ${familiar.State}`;
-  // const text = `Position: ${familiar.Position.X}, ${familiar.Position.Y}`;
-  // const text = `Animation: ${familiar.GetSprite().GetAnimation()}`;
-  // const text = `Visible: ${familiar.IsVisible()}`;
-  // const position = Isaac.WorldToScreen(familiar.Position);
-  // Isaac.RenderText(text, position.X, position.Y, 1, 1, 1, 1);
+  /*
+  const text = `State: ${familiar.State}`;
+  const text = `Position: ${familiar.Position.X}, ${familiar.Position.Y}`;
+  const text = `Animation: ${familiar.GetSprite().GetAnimation()}`;
+  const text = `Visible: ${familiar.IsVisible()}`;
+  const position = Isaac.WorldToScreen(familiar.Position);
+  Isaac.RenderText(text, position.X, position.Y, 1, 1, 1, 1);
+  */
 }
 
 // ModCallbacks.MC_POST_NPC_RENDER (28)
@@ -62,28 +64,33 @@ export function postNPCRender(_npc: EntityNPC): void {
   if (npc.Parent !== undefined) {
     return;
   }
+
+  const text = `State: ${npc.State}, StateFrame: ${npc.StateFrame}, I1: ${npc.I1}, I2: ${npc.I2}, V1: ${npc.V1}, V2: ${npc.V2}`;
+  const sprite = npc.GetSprite();
+  const animation = sprite.GetAnimation();
+  const text = `Animation: ${animation}`;
+  const position = Isaac.WorldToScreen(npc.Position);
+  Isaac.RenderText(text, position.X, position.Y, 1, 1, 1, 1);
   */
-  // const text = `State: ${npc.State}, StateFrame: ${npc.StateFrame}, I1: ${npc.I1}, I2: ${npc.I2}, V1: ${npc.V1}, V2: ${npc.V2}`;
-  // const sprite = npc.GetSprite();
-  // const animation = sprite.GetAnimation();
-  // const text = `Animation: ${animation}`;
-  // const position = Isaac.WorldToScreen(npc.Position);
-  // Isaac.RenderText(text, position.X, position.Y, 1, 1, 1, 1);
 }
 
 // ModCallbacks.MC_POST_PLAYER_RENDER (32)
-export function postPlayerRender(player: EntityPlayer): void {
+export function postPlayerRender(_player: EntityPlayer): void {
+  /*
   const x = round(player.Velocity.X, 1);
   const y = round(player.Velocity.Y, 1);
   const text = `Velocity: ${x}, ${y}`;
   const position = Isaac.WorldToScreen(player.Position);
   Isaac.RenderText(text, position.X, position.Y, 1, 1, 1, 1);
+  */
 }
 
 // ModCallbacks.MC_POST_EFFECT_RENDER (56)
 export function postEffectRender(_effect: EntityEffect): void {
-  // const text = `State: ${effect.State}`;
-  // const text = `Position: ${effect.Position.X}, ${effect.Position.Y}`;
-  // const position = Isaac.WorldToScreen(effect.Position);
-  // Isaac.RenderText(text, position.X, position.Y, 1, 1, 1, 1);
+  /*
+  const text = `State: ${effect.State}`;
+  const text = `Position: ${effect.Position.X}, ${effect.Position.Y}`;
+  const position = Isaac.WorldToScreen(effect.Position);
+  Isaac.RenderText(text, position.X, position.Y, 1, 1, 1, 1);
+  */
 }
