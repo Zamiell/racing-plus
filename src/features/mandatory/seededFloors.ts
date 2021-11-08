5444863a19e1f44205ff73a7950561c5d51ab66f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-08 10:47:19 -0500

    updating deps

--- src/features/mandatory/seededFloors.ts
+++ src/features/mandatory/seededFloors.ts
@@ -2,25 +2,17 @@
 // This feature relies on fast travel to function
 
 import {
-  CHARACTERS_WITH_NO_RED_HEARTS,
+  getPlayerHealth,
   getRandom,
   isKeeper,
   nextSeed,
   onSetSeed,
+  PlayerHealth,
   saveDataManager,
+  setPlayerHealth,
 } from "isaacscript-common";
 import g from "../../globals";
 import { config } from "../../modConfigMenu";
-
-interface Health {
-  soulHeartTypes: HeartSubType[];
-  maxHearts: int;
-  hearts: int;
-  soulHearts: int;
-  boneHearts: int;
-  goldenHearts: int;
-  rottenHearts: int;
-}
 
 interface GameStateFlags {
   devilVisited: boolean;
@@ -37,7 +29,7 @@ const v = {
     swapping: false,
     gameStateFlags: null as GameStateFlags | null,
     inventory: null as Inventory | null,
-    health: null as Health | null,
+    playerHealth: null as PlayerHealth | null,
   },
 };
 
@@ -85,7 +77,7 @@ export function before(stage: int): void {
   v.run.swapping = true;
   v.run.gameStateFlags = getGameStateFlags();
   v.run.inventory = getInventory(player);
-  v.run.health = getHealth(player);
+  v.run.playerHealth = getPlayerHealth(player);
 
   // Modification 1: Devil Room visited
   if (stage < 3) {
@@ -182,9 +174,11 @@ export function after(): void {
   if (v.run.inventory !== null) {
     setInventory(player, v.run.inventory);
   }
-  if (v.run.health !== null) {
-    setHealth(player, v.run.health);
+  if (v.run.playerHealth !== null) {
+    setPlayerHealth(player, v.run.playerHealth);
   }
+
+  g.seeds.RemoveSeedEffect(SeedEffect.SEED_PERMANENT_CURSE_UNKNOWN);
 }
 
 function getGameStateFlags(): GameStateFlags {
@@ -223,151 +217,4 @@ function setInventory(player: EntityPlayer, inventory: Inventory) {
   player.AddCoins(inventory.coins);
   player.AddKeys(-99);
   player.AddKeys(inventory.keys);
-}
-
-// Based on the "REVEL.StoreHealth()" function in the Revelations mod
-function getHealth(player: EntityPlayer): Health {
-  const character = player.GetPlayerType();
-  const soulHeartTypes: HeartSubType[] = [];
-  let maxHearts = player.GetMaxHearts();
-  let hearts = player.GetHearts();
-  let soulHearts = player.GetSoulHearts();
-  let boneHearts = player.GetBoneHearts();
-  const goldenHearts = player.GetGoldenHearts();
-  const eternalHearts = player.GetEternalHearts();
-  const rottenHearts = player.GetRottenHearts();
-  const subPlayer = player.GetSubPlayer();
-
-  // The Forgotten and The Soul has special health, so we need to account for this
-  if (character === PlayerType.PLAYER_THEFORGOTTEN && subPlayer !== undefined) {
-    // The Forgotten does not have red heart containers
-    maxHearts = boneHearts * 2;
-    boneHearts = 0;
-
-    // The Forgotten will always have 0 soul hearts;
-    // we need to get the soul heart amount from the sub player
-    soulHearts = subPlayer.GetSoulHearts();
-  } else if (
-    character === PlayerType.PLAYER_THESOUL &&
-    subPlayer !== undefined
-  ) {
-    // The Soul will always have 0 bone hearts;
-    // we need to get the bone heart amount from the sub player
-    // We need to store it as "maxHearts" instead of "boneHearts"
-    maxHearts = subPlayer.GetBoneHearts() * 2;
-    hearts = subPlayer.GetHearts();
-  }
-
-  // Eternal Hearts will be lost since we are about to change floors,
-  // so convert it to other types of health
-  // "eternalHearts" will be equal to 1 if we have an Eternal Heart
-  if (CHARACTERS_WITH_NO_RED_HEARTS.has(character)) {
-    soulHearts += eternalHearts * 2;
-  } else {
-    maxHearts += eternalHearts * 2;
-    hearts += eternalHearts * 2;
-  }
-
-  // This is the number of individual hearts shown in the HUD, minus heart containers
-  const extraHearts = math.ceil(soulHearts / 2) + boneHearts;
-
-  // Since bone hearts can be inserted anywhere between soul hearts,
-  // we need a separate counter to track which soul heart we're currently at
-  let currentSoulHeart = 0;
-
-  for (let i = 0; i < extraHearts; i++) {
-    let isBoneHeart = player.IsBoneHeart(i);
-    if (
-      character === PlayerType.PLAYER_THEFORGOTTEN &&
-      subPlayer !== undefined
-    ) {
-      // 16
-      isBoneHeart = subPlayer.IsBoneHeart(i);
-    }
-    if (isBoneHeart) {
-      soulHeartTypes.push(HeartSubType.HEART_BONE);
-    } else {
-      // We need to add 1 here because only the second half of a black heart is considered black
-      let isBlackHeart = player.IsBlackHeart(currentSoulHeart + 1);
-      if (
-        character === PlayerType.PLAYER_THEFORGOTTEN &&
-        subPlayer !== undefined
-      ) {
-        // 16
-        isBlackHeart = subPlayer.IsBlackHeart(currentSoulHeart + 1);
-      }
-      if (isBlackHeart) {
-        soulHeartTypes.push(HeartSubType.HEART_BLACK);
-      } else {
-        soulHeartTypes.push(HeartSubType.HEART_SOUL);
-      }
-
-      // Move to the next heart
-      currentSoulHeart += 2;
-    }
-  }
-
-  return {
-    soulHeartTypes,
-    maxHearts,
-    hearts,
-    soulHearts,
-    boneHearts,
-    goldenHearts,
-    rottenHearts,
-  };
-}
-
-// Based on the "REVEL.LoadHealth()" function in the Revelations mod
-function setHealth(player: EntityPlayer, health: Health) {
-  const character = player.GetPlayerType();
-  const subPlayer = player.GetSubPlayer();
-
-  // Remove all existing health
-  player.AddMaxHearts(-24, true);
-  player.AddSoulHearts(-24);
-  player.AddBoneHearts(-24);
-
-  // Add the red heart containers
-  if (character === PlayerType.PLAYER_THESOUL && subPlayer !== undefined) {
-    // Adding health to The Soul is a special case
-    subPlayer.AddMaxHearts(health.maxHearts, false);
-  } else {
-    player.AddMaxHearts(health.maxHearts, false);
-  }
-
-  // Add the soul / black / bone hearts
-  let soulHeartsRemaining = health.soulHearts;
-  for (let i = 1; i <= health.soulHeartTypes.length; i++) {
-    const heartType = health.soulHeartTypes[i - 1];
-    const isHalf = health.soulHearts + health.boneHearts * 2 < i * 2;
-    let addAmount = 2;
-    if (
-      isHalf ||
-      heartType === HeartSubType.HEART_BONE ||
-      soulHeartsRemaining < 2
-    ) {
-      // Fix the bug where a half soul heart to the left of a bone heart will be treated as a full
-      // soul heart
-      addAmount = 1;
-    }
-
-    if (heartType === HeartSubType.HEART_SOUL) {
-      player.AddSoulHearts(addAmount);
-      soulHeartsRemaining -= addAmount;
-    } else if (heartType === HeartSubType.HEART_BLACK) {
-      player.AddBlackHearts(addAmount);
-      soulHeartsRemaining -= addAmount;
-    } else if (heartType === HeartSubType.HEART_BONE) {
-      player.AddBoneHearts(addAmount);
-    }
-  }
-
-  // Fill in the red heart containers
-  player.AddHearts(health.hearts);
-  player.AddGoldenHearts(health.goldenHearts);
-  // (no matter what kind of heart is added, no sounds effects will play)
-  player.AddRottenHearts(health.rottenHearts);
-
-  g.seeds.RemoveSeedEffect(SeedEffect.SEED_PERMANENT_CURSE_UNKNOWN);
 }
