2db6f7b1614bfb8958670ce4956ad80c1e0465f9
Rchardon <33094562+Rchardon@users.noreply.github.com>
2021-11-15 13:52:00 +0100

    Remove gello fix

--- src/callbacks/postGameStarted.ts
+++ src/callbacks/postGameStarted.ts
@@ -3,7 +3,6 @@ import * as centerStart from "../features/mandatory/centerStart";
 import * as disableMultiplayer from "../features/mandatory/disableMultiplayer";
 import * as errors from "../features/mandatory/errors";
 import * as fireworks from "../features/mandatory/fireworks";
-import * as gelloSaveAndQuitFix from "../features/mandatory/gelloSaveAndQuitFix";
 import * as genesisSaveAndQuitFix from "../features/mandatory/genesisSaveAndQuitFix";
 import * as modConfigNotify from "../features/mandatory/modConfigNotify";
 import * as racingPlusSprite from "../features/mandatory/racingPlusSprite";
@@ -105,5 +104,4 @@ export function main(isContinued: boolean): void {
 function postGameStartedContinued() {
   fastTravelPostGameStartedContinued();
   genesisSaveAndQuitFix.postGameStartedContinued();
-  gelloSaveAndQuitFix.postGameStartedContinued();
 }
