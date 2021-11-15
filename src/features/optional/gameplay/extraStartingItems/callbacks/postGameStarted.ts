db8ad24e2fe853a680ec8dc8c1d3de855213ff59
Rchardon <33094562+Rchardon@users.noreply.github.com>
2021-11-15 16:14:26 +0100

    Revert "remove replace placeholders"
    This reverts commit 10c14005c70680cbdc16de47d3ec565d31ba17e6.

--- src/features/optional/gameplay/extraStartingItems/callbacks/postGameStarted.ts
+++ src/features/optional/gameplay/extraStartingItems/callbacks/postGameStarted.ts
@@ -1,10 +1,12 @@
 import { config } from "../../../../../modConfigMenu";
 import * as extraStartingItems from "../extraStartingItems";
+import * as replacePlaceholdersOnEden from "../replacePlaceholdersOnEden";
 
 export function extraStartingItemsPostGameStarted(): void {
   if (!config.extraStartingItems) {
     return;
   }
 
+  replacePlaceholdersOnEden.postGameStarted();
   extraStartingItems.postGameStarted();
 }
