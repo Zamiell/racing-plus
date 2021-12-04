012fb7675c94eb0492f6ce51945b0b3cccef1c17
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-03 20:15:59 -0500

    bugfix for seeded death removing wisps

--- src/features/race/seededDeathDebuff.ts
+++ src/features/race/seededDeathDebuff.ts
@@ -7,75 +7,18 @@ import {
   getPlayerCollectibleMap,
   getTransformationsForItem,
   isJacobOrEsau,
+  removeAllFamiliars,
   removeAllPlayerHealth,
   removeCollectibleFromItemTracker,
   removeDeadEyeMultiplier,
 } from "isaacscript-common";
 import g from "../../globals";
+import { TRANSFORMATION_TO_HELPER_MAP } from "../../maps/transformationToHelperMap";
 import { ActiveItemDescription } from "../../types/ActiveItemDescription";
-import {
-  CollectibleTypeCustom,
-  TRANSFORMATION_HELPERS,
-} from "../../types/enums";
+import { TRANSFORMATION_HELPERS } from "../../types/enums";
 import v from "./v";
 
 const QUARTER_FADED_COLOR = Color(1, 1, 1, 0.25);
-
-const TRANSFORMATION_TO_HELPER_MAP = new Map<PlayerForm, CollectibleTypeCustom>(
-  [
-    [
-      PlayerForm.PLAYERFORM_GUPPY, // 0
-      CollectibleTypeCustom.COLLECTIBLE_GUPPY_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_LORD_OF_THE_FLIES, // 1
-      CollectibleTypeCustom.COLLECTIBLE_BEELZEBUB_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_MUSHROOM, // 2
-      CollectibleTypeCustom.COLLECTIBLE_FUN_GUY_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_ANGEL, // 3
-      CollectibleTypeCustom.COLLECTIBLE_SERAPHIM_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_BOB, // 4
-      CollectibleTypeCustom.COLLECTIBLE_BOB_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_DRUGS, // 5
-      CollectibleTypeCustom.COLLECTIBLE_SPUN_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_MOM, // 6
-      CollectibleTypeCustom.COLLECTIBLE_YES_MOTHER_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_BABY, // 7
-      CollectibleTypeCustom.COLLECTIBLE_CONJOINED_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_EVIL_ANGEL, // 8
-      CollectibleTypeCustom.COLLECTIBLE_LEVIATHAN_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_POOP, // 9
-      CollectibleTypeCustom.COLLECTIBLE_OH_CRAP_TRANSFORMATION_HELPER,
-    ],
-    [
-      PlayerForm.PLAYERFORM_BOOK_WORM, // 10
-      CollectibleTypeCustom.COLLECTIBLE_BOOKWORM_TRANSFORMATION_HELPER,
-    ],
-    // PlayerForm.PLAYERFORM_ADULTHOOD (11) is skipped since that is not based on items
-    [
-      PlayerForm.PLAYERFORM_SPIDERBABY, // 12
-      CollectibleTypeCustom.COLLECTIBLE_SPIDER_BABY_TRANSFORMATION_HELPER,
-    ],
-    // PlayerForm.PLAYERFORM_STOMPY (13) is skipped since that is not based on items
-    // Playerform.PLAYERFORM_FLIGHT (14) is unused by the game
-  ],
-);
 
 export function debuffOn(player: EntityPlayer): void {
   // Make them take "red heart damage" for the purposes of getting a Devil Deal
@@ -86,6 +29,7 @@ export function debuffOn(player: EntityPlayer): void {
   debuffOnRemoveActiveItems(player);
   debuffOnRemoveAllItems(player);
   debuffOnRemoveGoldenBombsAndKeys(player);
+  debuffOnRemoveAllWisps();
   removeDeadEyeMultiplier(player);
   debuffOnRemoveDarkEsau();
 }
@@ -221,6 +165,11 @@ function debuffOnRemoveGoldenBombsAndKeys(player: EntityPlayer) {
   // Remove any golden bombs and keys
   player.RemoveGoldenBomb();
   player.RemoveGoldenKey();
+}
+
+function debuffOnRemoveAllWisps() {
+  removeAllFamiliars(FamiliarVariant.WISP);
+  removeAllFamiliars(FamiliarVariant.ITEM_WISP);
 }
 
 function debuffOnRemoveDarkEsau() {
---
+++ src/maps/transformationToHelperMap.ts
@@ -0,0 +1,58 @@
+import { CollectibleTypeCustom } from "../types/enums";
+
+export const TRANSFORMATION_TO_HELPER_MAP = new Map<
+  PlayerForm,
+  CollectibleTypeCustom
+>([
+  [
+    PlayerForm.PLAYERFORM_GUPPY, // 0
+    CollectibleTypeCustom.COLLECTIBLE_GUPPY_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_LORD_OF_THE_FLIES, // 1
+    CollectibleTypeCustom.COLLECTIBLE_BEELZEBUB_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_MUSHROOM, // 2
+    CollectibleTypeCustom.COLLECTIBLE_FUN_GUY_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_ANGEL, // 3
+    CollectibleTypeCustom.COLLECTIBLE_SERAPHIM_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_BOB, // 4
+    CollectibleTypeCustom.COLLECTIBLE_BOB_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_DRUGS, // 5
+    CollectibleTypeCustom.COLLECTIBLE_SPUN_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_MOM, // 6
+    CollectibleTypeCustom.COLLECTIBLE_YES_MOTHER_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_BABY, // 7
+    CollectibleTypeCustom.COLLECTIBLE_CONJOINED_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_EVIL_ANGEL, // 8
+    CollectibleTypeCustom.COLLECTIBLE_LEVIATHAN_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_POOP, // 9
+    CollectibleTypeCustom.COLLECTIBLE_OH_CRAP_TRANSFORMATION_HELPER,
+  ],
+  [
+    PlayerForm.PLAYERFORM_BOOK_WORM, // 10
+    CollectibleTypeCustom.COLLECTIBLE_BOOKWORM_TRANSFORMATION_HELPER,
+  ],
+  // PlayerForm.PLAYERFORM_ADULTHOOD (11) is skipped since that is not based on items
+  [
+    PlayerForm.PLAYERFORM_SPIDERBABY, // 12
+    CollectibleTypeCustom.COLLECTIBLE_SPIDER_BABY_TRANSFORMATION_HELPER,
+  ],
+  // PlayerForm.PLAYERFORM_STOMPY (13) is skipped since that is not based on items
+  // Playerform.PLAYERFORM_FLIGHT (14) is unused by the game
+]);
