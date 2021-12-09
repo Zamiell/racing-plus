31544c0b79354543de6f0f2e5894ee1f225b78d1
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-09 08:23:22 -0500

    updating to prettier

--- lint.sh
+++ lint.sh
@@ -17,16 +17,19 @@ SECONDS=0
 
 cd "$DIR"
 
-# Step 1 - Use ESLint to lint the TypeScript
+# Step 1 - Use Prettier to check formatting
+npx prettier --check "src/**/*.ts"
+
+# Step 2 - Use ESLint to lint the TypeScript
 # Since all ESLint errors are set to warnings,
 # we set max warnings to 0 so that warnings will fail in CI
 npx eslint --max-warnings 0 src
 
-# Step 2 - Spell check every file using cspell
+# Step 3 - Spell check every file using cspell
 # We use no-progress and no-summary because we want to only output errors
 npx cspell --no-progress --no-summary "src/**/*.ts"
-npx cspell --no-progress --no-summary "docs/**/*.md"
 npx cspell --no-progress --no-summary "mod/metadata.xml"
+npx cspell --no-progress --no-summary "docs/**/*.md"
 
 # Step 3 - Check for unused imports
 # The "--error" flag makes it return an error code of 1 if unused exports are found
