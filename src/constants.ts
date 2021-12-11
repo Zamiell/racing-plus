7cb0e5faab01df7c954e2a962276a06a7b97a7dc
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-10 22:16:53 -0500

    v0.74.4

--- src/constants.ts
+++ src/constants.ts
@@ -13,4 +13,4 @@ export const SPRITE_BETHANY_OFFSET = Vector(0, 10);
 export const SPRITE_JACOB_ESAU_OFFSET = Vector(0, 15);
 
 /** The version is updated automatically by a pre-publish script. */
-export const VERSION = "0.74.2";
+export const VERSION = "0.74.4";
