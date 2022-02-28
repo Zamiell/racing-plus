af4637b0804d28af57e21e0b8235362842abb1eb
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-02-28 02:11:18 -0500

    cleanup dead code

--- src/features/speedrun/season2/callbacks/postNewRoom.ts
+++ src/features/speedrun/season2/callbacks/postNewRoom.ts
@@ -1,8 +1,5 @@
-import { getPickups } from "isaacscript-common";
-import g from "../../../../globals";
 import { getRoomsEntered } from "../../../utils/roomsEntered";
 import { ChallengeCustom } from "../../enums";
-import { speedrunGetCharacterNum } from "../../exported";
 import { resetSprites } from "../sprites";
 
 export function season2PostNewRoom(): void {
@@ -17,27 +14,4 @@ export function season2PostNewRoom(): void {
   if (roomsEntered !== 1) {
     resetSprites();
   }
-
-  preventResettingForCurseRoom(roomsEntered);
-}
-
-function preventResettingForCurseRoom(roomsEntered: int) {
-  const characterNum = speedrunGetCharacterNum();
-  const roomType = g.r.GetType();
-
-  if (
-    characterNum !== 1 ||
-    roomType !== RoomType.ROOM_CURSE ||
-    roomsEntered !== 2
-  ) {
-    return;
-  }
-
-  const pickups = getPickups();
-  for (const pickup of pickups) {
-    pickup.Remove();
-  }
-
-  const player = Isaac.GetPlayer();
-  player.AnimateSad();
 }
