ed6f5e21233565045bf22a5b799a087942e58a10
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-12 19:23:03 -0500

    v0.78.16

--- src/features/items/debug.ts
+++ src/features/items/debug.ts
@@ -1,5 +1,7 @@
 import { debugFunction } from "../../debugFunction";
 
+// ModCallbacks.MC_USE_ITEM (3)
+// CollectibleTypeCustom.COLLECTIBLE_DEBUG
 export function useItemDebug(): boolean {
   debugFunction();
   return true; // Display the "use" animation
