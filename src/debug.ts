d184c3e1c86c03f0fed66bb18eef82ec0aacb333
James <5511220+Zamiell@users.noreply.github.com>
2022-07-26 13:59:28 -0400

    chore: cleanup

--- src/debug.ts
+++ src/debug.ts
@@ -1,24 +1,10 @@
-import { HeartSubType, Keyboard } from "isaac-typescript-definitions";
-import {
-  newPlayerHealth,
-  printConsole,
-  setPlayerHealth,
-} from "isaacscript-common";
+import { Keyboard } from "isaac-typescript-definitions";
+import { printConsole } from "isaacscript-common";
 import { hotkeys } from "./modConfigMenu";
 
 /** Currently, F2 is set up to execute this function. */
 export function debugCode(_params?: string): void {
   // Add code here.
-  const playerHealth = {
-    ...newPlayerHealth(),
-    maxHearts: 2,
-    hearts: 1,
-    soulHearts: 1,
-    soulHeartTypes: [HeartSubType.SOUL],
-  };
-
-  const player = Isaac.GetPlayer();
-  setPlayerHealth(player, playerHealth);
 }
 
 export function hotkey1Function(): void {
