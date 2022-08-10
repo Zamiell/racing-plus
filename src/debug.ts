5f6aeb11c6940fddefce80b65cbc56ccdd876ba2
James <5511220+Zamiell@users.noreply.github.com>
2022-08-10 16:56:25 -0400

    chore: updating deps

--- src/debug.ts
+++ src/debug.ts
@@ -13,11 +13,13 @@ function debugCode(_params?: string) {
   // Add code here.
 }
 
+/** Hotkey 1 is bound to F2. */
 export function hotkey1Function(): void {
   printConsole("Hotkey 1 activated.");
   debugCode();
 }
 
+/** Hotkey 2 is bound to F3. */
 export function hotkey2Function(): void {
   hotkeys.fastDropAll = Keyboard.Z;
   hotkeys.autofire = Keyboard.F;
