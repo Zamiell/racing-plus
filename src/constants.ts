79114ef06d139fdec35654b50437b63f6f4e0d59
James <5511220+Zamiell@users.noreply.github.com>
2022-08-26 16:20:27 -0400

    chore: release v0.84.4

--- src/constants.ts
+++ src/constants.ts
@@ -9,4 +9,4 @@ export const SPRITE_BETHANY_OFFSET = Vector(0, 10);
 export const SPRITE_JACOB_ESAU_OFFSET = Vector(0, 15);
 
 /** The version is updated automatically by the IsaacScript publish command. */
-export const VERSION = "0.84.3";
+export const VERSION = "0.84.4";
