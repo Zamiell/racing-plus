89424fdb1101615bedd285d250fbfb9b9ca7cf88
James <5511220+Zamiell@users.noreply.github.com>
2022-09-08 01:52:34 -0400

    fix: ChallengeCustom

--- src/enums/ChallengeCustom.ts
+++ src/enums/ChallengeCustom.ts
@@ -3,7 +3,7 @@ import { validateCustomEnum } from "isaacscript-common";
 export const ChallengeCustom = {
   SEASON_1: Isaac.GetChallengeIdByName("R+7 Season 1"),
   SEASON_2: Isaac.GetChallengeIdByName("R+7 Season 2"),
-  SEASON_3: Isaac.GetChallengeIdByName("R+7 Season 3 Beta"),
+  SEASON_3: Isaac.GetChallengeIdByName("R+7 Season 3"),
   CHANGE_CHAR_ORDER: Isaac.GetChallengeIdByName("Change Char Order"),
 } as const;
 
