b6fcc42990c93050ab641fa18431f2b00c0c2268
James <5511220+Zamiell@users.noreply.github.com>
2022-12-28 16:13:10 -0500

    fix: cspell gitignore

--- lint.sh
+++ lint.sh
@@ -19,10 +19,7 @@ npx eslint --max-warnings 0 .
 
 # Step 3 - Spell check every file using CSpell.
 # We use "--no-progress" and "--no-summary" because we want to only output errors.
-# We use "--gitignore" because we want to ignore files which will are not included in the repo.
-# (The VS Code extension ignores gitignore files by default, so we want the CLI to be unified with
-# the editor.)
-npx cspell --no-progress --no-summary --gitignore
+npx cspell --no-progress --no-summary
 
 # Step 4 - Check for orphaned words.
 bash "$DIR/check-orphaned-words.sh"
