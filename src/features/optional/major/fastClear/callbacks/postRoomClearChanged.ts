807fd82aa0ea7cdbe57c8589924f78573b2e3ad5
James <5511220+Zamiell@users.noreply.github.com>
2022-12-29 00:57:40 -0500

    chore: log cleanup

--- src/features/optional/major/fastClear/callbacks/postRoomClearChanged.ts
+++ src/features/optional/major/fastClear/callbacks/postRoomClearChanged.ts
@@ -2,6 +2,7 @@ import { log } from "isaacscript-common";
 import { shouldEnableFastClear } from "../shouldEnableFastClear";
 import v from "../v";
 
+/** This intentionally does not use the `PRE_SPAWN_CLEAR_AWARD` callback. */
 export function fastClearPostRoomClearChanged(roomClear: boolean): void {
   if (!shouldEnableFastClear()) {
     return;
@@ -11,9 +12,6 @@ export function fastClearPostRoomClearChanged(roomClear: boolean): void {
 }
 
 function checkVanillaRoomClear(roomClear: boolean) {
-  const verb = roomClear ? "cleared" : "uncleared";
-  log(`Room state changed to: ${verb}`);
-
   if (roomClear && !v.room.fastClearedRoom) {
     log("Vanilla room clear detected.");
   }
