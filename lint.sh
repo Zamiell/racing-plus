bcded9e3bf509a7ce2576658d02a2f46005b374d
James <5511220+Zamiell@users.noreply.github.com>
2023-01-16 11:26:55 -0500

    chore: update lint.sh

--- lint.sh
+++ lint.sh
@@ -17,23 +17,23 @@ npx prettier --check .
 # We use "--max-warnings" so that any warnings will fail in CI.
 npx eslint --max-warnings 0 .
 
-# Step 3 - Use `isaac-xml-validator` to validate XML files.
+# Step 3 - Check for unused imports.
+# The "--error" flag makes it return an error code of 1 if unused exports are found.
+npx ts-prune --error
+
+# Step 4 - Use `isaac-xml-validator` to validate XML files.
 # (Skip this step if Python is not currently installed for whatever reason.)
 if command -v python &> /dev/null; then
   pip install isaac-xml-validator --upgrade
   isaac-xml-validator
 fi
 
-# Step 4 - Spell check every file using CSpell.
+# Step 5 - Spell check every file using CSpell.
 # We use "--no-progress" and "--no-summary" because we want to only output errors.
 npx cspell --no-progress --no-summary
 
-# Step 5 - Check for orphaned words.
+# Step 6 - Check for orphaned words.
 bash "$DIR/check-orphaned-words.sh"
-
-# Step 6 - Check for unused imports.
-# The "--error" flag makes it return an error code of 1 if unused exports are found.
-npx ts-prune --error
 
 # Step 7 - Check for base file updates.
 bash "$DIR/check-file-updates.sh"
