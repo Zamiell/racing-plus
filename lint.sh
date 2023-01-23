c85a3d252898db2408a4cfd76a70dcbf6ce80df6
James <5511220+Zamiell@users.noreply.github.com>
2023-01-23 15:46:42 -0500

    chore: update deps

--- lint.sh
+++ lint.sh
@@ -30,7 +30,7 @@ fi
 
 # Step 5 - Spell check every file using CSpell.
 # We use "--no-progress" and "--no-summary" because we want to only output errors.
-npx cspell --no-progress --no-summary
+npx cspell --no-progress --no-summary .
 
 # Step 6 - Check for orphaned words.
 bash "$DIR/check-orphaned-words.sh"
