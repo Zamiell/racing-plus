c11dc29e62aec13c71484a0d356536ab2d06c4d5
James <5511220+Zamiell@users.noreply.github.com>
2023-02-03 13:31:19 -0500

    fix: lint

--- lint.sh
+++ lint.sh
@@ -10,32 +10,37 @@ SECONDS=0
 
 cd "$DIR"
 
-# Step 1 - Use Prettier to check formatting.
-npx prettier --check .
+# Use Prettier to check formatting.
+# "--loglevel warn" makes it only output errors.
+npx prettier --loglevel warn --check .
 
-# Step 2 - Use ESLint to lint the TypeScript.
-# We use "--max-warnings" so that any warnings will fail in CI.
+# Use ESLint to lint the TypeScript.
+# "--max-warnings 0" makes warnings fail in CI, since we set all ESLint errors to warnings.
 npx eslint --max-warnings 0 .
 
-# Step 3 - Check for unused imports.
-# The "--error" flag makes it return an error code of 1 if unused exports are found.
+# Check for unused imports.
+# "--error" makes it return an error code of 1 if unused exports are found.
 npx ts-prune --error
 
-# Step 4 - Use `isaac-xml-validator` to validate XML files.
+# Use `isaac-xml-validator` to validate XML files.
 # (Skip this step if Python is not currently installed for whatever reason.)
 if command -v python &> /dev/null; then
   pip install isaac-xml-validator --upgrade --quiet
-  #isaac-xml-validator
+  # @template-ignore-next-line
+  isaac-xml-validator --quiet --ignore cutscenes.xml
 fi
 
-# Step 5 - Spell check every file using CSpell.
-# We use "--no-progress" and "--no-summary" because we want to only output errors.
+# Spell check every file using CSpell.
+# "--no-progress" and "--no-summary" make it only output errors.
 npx cspell --no-progress --no-summary .
 
-# Step 6 - Check for orphaned words.
+# Check for orphaned words.
 bash "$DIR/check-orphaned-words.sh"
 
-# Step 7 - Check for base file updates.
-bash "$DIR/check-file-updates.sh"
+# @template-customization-start
+#  Check for base file updates.
+#bash "$DIR/check-file-updates.sh" # TODO
+#npx isaacscript check # TODO
+# @template-customization-end
 
 echo "Successfully linted in $SECONDS seconds."
