8e3a7482afa47df3cc1959e316827464fc3411e0
James <5511220+Zamiell@users.noreply.github.com>
2023-02-07 13:58:55 -0500

    fix: line endings

--- lint.sh
+++ lint.sh
@@ -38,8 +38,36 @@ npx cspell --no-progress --no-summary .
 bash "$DIR/check-orphaned-words.sh"
 
 # @template-customization-start
-#  Check for base file updates.
+
+# Check for base file updates.
 npx isaacscript check
+
+# Check for Windows-style line endings.
+WINDOWS_FILES=$(
+  grep \
+  --recursive \
+  --files-with-matches \
+  --binary \
+  --perl-regexp \
+  --exclude-dir='.git' \
+  --exclude-dir='node_modules' \
+  --exclude-dir='rooms' \
+  --exclude-dir='pre-flipped' \
+  --exclude='*.fnt' \
+  --exclude='*.png' \
+  --exclude='*.pyc' \
+  --exclude='*.stb' \
+  --exclude='*.ttf' \
+  --exclude='*.wav' \
+  --exclude='yarn.lock' \
+  '\r$' .
+) || true
+if [[ $WINDOWS_FILES ]]; then
+  echo "Files with Windows line-endings were found:"
+  echo "$WINDOWS_FILES"
+  exit 1
+fi
+
 # @template-customization-end
 
 echo "Successfully linted in $SECONDS seconds."
